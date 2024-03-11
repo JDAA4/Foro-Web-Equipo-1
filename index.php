1fab55aa9dceea0b7b092d4642556d72573f0679
Alan-San-Millan <asanmillan@ucol.mx>
2024-03-11 11:08:36 -0600

    Modificaciones menores al main

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-header("location:auth/login.html");
+header("location:src/views/login.php");
 ?>
--- src/views/main.php
+++ src/views/main.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -46,7 +45,7 @@
         <script src="script.js"></script>
 
 
-        <a href="./login.php" class="logout">Iniciar Sesión</a>
+        <a href="../../controllers/logout.php" class="logout">Cerrar Sesión</a>
       </div>
 </div>
 
@@ -163,10 +162,7 @@
               <a href="../views/user-profile.html"><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-              </svg>  Perfil</button><a/>
-            <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
-                <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3q0-.405-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z"/>
-              </svg>  Configuracion</button>
+              </svg>  Perfil</button></a>
         </div>
         <div class="forum">
           <button><img src="../images/6.png"></button>
@@ -175,5 +171,11 @@
         </div>
     </div>
 </div>
+<button class="toTop" id="toTop">
+  <svg viewBox="0 0 24 24">
+    <path d="m4 16 8-8 8 8"></path>
+  </svg>
+</button>
+<script src="../js/script.js"></script>
 </body>
 </html>
